da34b37f8cdd3d946a9e6b47bc015de175d6783d
Alex <alex1999099@gmail.com>
2021-07-23 12:00:20 +0300

    Create Storage layer for Azure Blob

---
+++ GhostNetwork.Gateway.Infrastructure/AzureBlob/AzureBlobStorage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Azure.Storage.Blobs;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostNetwork.Gateway.AzureBlob
+{
+    class AzureBlobStorage : IAzureBlobStorage
+    {
+
+        private BlobContainerClient BlobContainerClient;
+
+        public AzureBlobStorage(string connectionString, string containerName)
+        {
+            BlobContainerClient = new BlobContainerClient(connectionString, containerName);
+
+
+        }
+        public async Task<(string, int)> CreateBlobAsync(string BlobName, Stream stream)
+        {
+            if(! await CreateContainer())
+            {
+                return (default(string), 2);
+            }
+
+            BlobClient blob = BlobContainerClient.GetBlobClient(BlobName);
+
+            if(await blob.ExistsAsync())
+            {
+                return (default(string), 1);
+            }
+
+            stream.Position = 0;
+
+            await blob.UploadAsync(stream);
+
+            return (blob.Uri.ToString(), default(int));
+
+        }
+
+        public async Task<int> DeleteBlobAsync(string BlobName)
+        {
+            if (!await CreateContainer())
+            {
+                return 2;
+            }
+
+            BlobClient blob = BlobContainerClient.GetBlobClient(BlobName);
+
+            if (await blob.ExistsAsync())
+            {
+                await blob.DeleteAsync();
+
+                return default(int);
+            }
+
+            return 3;
+        }
+
+        public async Task<(Stream, int)> GetStreamBlobAsync(string BlobName)
+        {
+            if (!await CreateContainer())
+            {
+                return (default(Stream), 2);
+            }
+
+            BlobClient blob = BlobContainerClient.GetBlobClient(BlobName);
+
+            if (! await blob.ExistsAsync())
+            {
+                return (default(Stream), 3);
+            }
+
+            MemoryStream downloaded = new MemoryStream();
+
+            await blob.DownloadToAsync(downloaded);
+
+            return (downloaded, 2);
+        }
+
+        private async Task<bool> CreateContainer()
+        {
+            try
+            {
+                await BlobContainerClient.CreateIfNotExistsAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ GhostNetwork.Gateway/AzureBlob/IAzureBlobStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GhostNetwork.Gateway.AzureBlob
+{
+    public interface IAzureBlobStorage
+    {
+        Task<(string, int)> CreateBlobAsync(string BlobName, Stream stream);
+
+        Task<(Stream,int)> GetStreamBlobAsync(string BlobName);
+
+        Task<int> DeleteBlobAsync(string BlobName);
+    }
+}
